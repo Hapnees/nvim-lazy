21646a4b3f73e9d2f4ad32eb470ffe71a5801dae
Hapnees <8dwardm8lton@gmail.com>
2023-08-29 16:46:32 +0300

    Feat (root): try to implement COC

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,16 +16,15 @@ local plugins = {
 }
 
 require("lazy").setup({
-	{ import = "plugins.autocomplete.plugin" },
 	{ import = "plugins.colorscheme.plugin" },
 	{ import = "plugins.neotree.plugin" },
 	{ import = "plugins.git.plugin" },
-	{ import = "plugins.lsp.plugin" },
 	{ import = "plugins.autoclosing.plugin" },
 	{ import = "plugins.telescope.plugin" },
 	{ import = "plugins.treesitter.plugin" },
 	{ import = "plugins.ui.plugin" },
 	{ import = "plugins.comment.plugin" },
 	{ import = "plugins.utils.plugin" },
+	{ import = "plugins.coc.plugin" },
 	plugins,
 })
